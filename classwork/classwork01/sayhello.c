dda8f7f84e4cbf4bcec3b91fcc965ebc2b0918b5
Coby Schumitzky <cobytyler7@gmail.com>
2022-09-01 12:55:08 -0700

    updated file to include challenge questions

--- classwork/classwork01/sayhello.c
+++ classwork/classwork01/sayhello.c
@@ -5,5 +5,7 @@ int main (int argc, char * argv[]) {
     char name[25];
     printf("Enter your name: ");
     gets( name );
-    printf("HELLO, %s", name);
+    printf("\n\n\t\t\t\t\t\tHello, %s\n\n", name);
+    // indenting using tab (\t)
+    // newlines using \n
 }
