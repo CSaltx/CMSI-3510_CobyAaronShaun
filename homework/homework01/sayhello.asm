678ab21ef75d86621710d2cca11f9d6834a06d13
Coby Schumitzky <cobytyler7@gmail.com>
2022-09-02 15:20:07 -0700

    updated sayhello.asm to include updated changes
    Updated sayhello.asm to include spacing challenge and newlines

--- homework/homework01/sayhello.asm
+++ homework/homework01/sayhello.asm
@@ -4,11 +4,12 @@
 start:   mov         rax, 0x02000004         ; system call for write [Linux should be "_start"]
          mov         rdi, 1                  ; file handle 1 is stdout
          mov         rsi, message            ; address of string to output
-         mov         rdx, 13                 ; number of bytes
+         mov         rdx, len                 ; number of bytes
          syscall                             ; invoke operating system to do the write
          mov         rax, 0x02000001         ; system call for exit [Linux use "60"]
          xor         rdi, rdi                ; exit code 0
          syscall                             ; invoke operating system to exit
 
          section     .data
-message: db          "Hello, World", 10
+message: db         `\n\n\t\t\t\t\t\tHello, World\n\n`
+len equ $ - message
