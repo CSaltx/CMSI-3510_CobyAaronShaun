ff536f58926cb7fc7ea03a8968da3c0798709e38
Coby Schumitzky <cobytyler7@gmail.com>
2022-09-06 14:18:57 -0700

    added more fun way of saying hello, name

--- homework/homework01/sayhello.c
+++ homework/homework01/sayhello.c
@@ -11,6 +11,9 @@ int main (int argc, char * argv[]) {
     printf("\n\n\t\t\t\t\t\tHello, %s\n\n", name);
     // indenting using tab (\t)
     // newlines using \n
+    
+    // for extra flair add this instead of a mere "Hello, name."
+    // printf( "\n\n\t\t\t\t\t\tHello, my dear fellow. I see your name is %s.\n\n Fun to meet you on this day!", name );
 
     return 0;
 
